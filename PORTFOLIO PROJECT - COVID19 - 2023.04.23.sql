27fb71356a7a0cd411a209f89e6800ca669c41e3
Robert Tuten <131642975+rtuten-analyst@users.noreply.github.com>
2023-04-24 01:39:17 -0500

    Update PORTFOLIO PROJECT - COVID19 - 2023.04.23.sql
    Adjusted Formatting.

--- PORTFOLIO PROJECT - COVID19 - 2023.04.23.sql
+++ PORTFOLIO PROJECT - COVID19 - 2023.04.23.sql
@@ -1,30 +1,42 @@
 -- **********************************************************************************************************************************
 -- Robert Tuten
 -- 2023.04.23
+-- DataSource: https://ourworldindata.org/covid-deaths
 
 USE PortfolioProject
 
 
 -- *** EXPLORATION ***
 
-
+--Checking Data:
 --SELECT * FROM PortfolioProject..covid_deaths
 --ORDER BY 3,4;
-
 
 --SELECT * FROM PortfolioProject..covid_vaccinations
 --ORDER BY 3,4;
 
 
-SELECT location, date, total_cases, new_cases, total_deaths, population FROM PortfolioProject..covid_deaths
+SELECT 
+	location, 
+	date, 
+	total_cases, 
+	new_cases, 
+	total_deaths, 
+	population 
+FROM PortfolioProject..covid_deaths
 ORDER BY 1,2;
 
 
 
 
 -- Looking at Total Cases vs. Total Deaths
--- Shows the likelihood of dying if you contract Covid-19 in your country
-SELECT location, date, total_cases, total_deaths, (CAST(total_deaths AS FLOAT) / CAST(total_cases AS FLOAT))*100 AS DeathPercentage 
+-- Shows the likelihood of dying if a person contracts Covid-19 in that country
+SELECT 
+	location, 
+	date, 
+	total_cases, 
+	total_deaths, 
+	(CAST(total_deaths AS FLOAT) / CAST(total_cases AS FLOAT))*100 AS DeathPercentage 
 FROM PortfolioProject..covid_deaths
 WHERE location like '%states' AND continent IS NOT NULL
 ORDER BY 1,2;
@@ -34,9 +46,13 @@ ORDER BY 1,2;
 
 -- Looking at Total Cases vs Population
 -- Shows what percentage of the population that caught Covid-19.
-SELECT location, date, population, total_cases, (CAST(total_cases AS FLOAT) / CAST(population AS FLOAT))*100 AS perfcentPopulationInfected 
+SELECT 
+	location, 
+	date, 
+	population, 
+	total_cases, 
+	(CAST(total_cases AS FLOAT) / CAST(population AS FLOAT))*100 AS perfcentPopulationInfected 
 FROM PortfolioProject..covid_deaths
--- WHERE location like '%states'
 WHERE continent IS NOT NULL
 ORDER BY 1,2;
 
@@ -44,9 +60,12 @@ ORDER BY 1,2;
 
 
 -- What countries have the highest infection rates compared to the population?
-SELECT location, population, MAX(total_cases) AS highest_infection_count, MAX(CAST(total_cases AS FLOAT) / CAST(population AS FLOAT))*100 AS perfcentPopulationInfected 
+SELECT 
+	location, 
+	population, 
+	MAX(total_cases) AS highest_infection_count, 
+	MAX(CAST(total_cases AS FLOAT) / CAST(population AS FLOAT))*100 AS perfcentPopulationInfected 
 FROM PortfolioProject..covid_deaths
--- WHERE location like '%states'
 WHERE continent IS NOT NULL
 GROUP BY population, location
 ORDER BY perfcentPopulationInfected DESC;
@@ -57,11 +76,10 @@ ORDER BY perfcentPopulationInfected DESC;
 -- Breakdown by CONTINENT
 SELECT location, MAX(total_deaths) AS totalDeathCount
 FROM PortfolioProject..covid_deaths
--- WHERE location like '%states'
 WHERE continent IS NULL AND location NOT LIKE ('%income%')
 GROUP BY location
 ORDER BY totalDeathCount DESC;
--- Currently contains 'Income Levels High, Upper, and Low as 'location' metrics'.
+-- Dataset contains 'Income Levels High, Upper, and Low as 'location' metrics'.
 
 
 
@@ -69,7 +87,6 @@ ORDER BY totalDeathCount DESC;
 -- Showing Countries with Highest Death Count per Population
 SELECT location, MAX(total_deaths) AS totalDeathCount
 FROM PortfolioProject..covid_deaths
--- WHERE location like '%states'
 WHERE continent IS NOT NULL
 GROUP BY location
 ORDER BY totalDeathCount DESC;
@@ -81,7 +98,6 @@ ORDER BY totalDeathCount DESC;
 -- Showing continents with the highest death count per population
 SELECT location, MAX(total_deaths) AS totalDeathCount
 FROM PortfolioProject..covid_deaths
--- WHERE location like '%states'
 WHERE continent IS NULL AND location NOT LIKE ('%income%')
 GROUP BY location
 ORDER BY totalDeathCount DESC;
@@ -89,7 +105,9 @@ ORDER BY totalDeathCount DESC;
 
 
 -- GLOBAL: The number of recorded cases and deaths as of 2023.04.23
-SELECT  SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, (SUM(CAST(new_deaths AS FLOAT))/SUM(CAST(new_cases AS FLOAT)))*100 AS globalDeathPercentage
+SELECT  
+	SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, 
+	(SUM(CAST(new_deaths AS FLOAT))/SUM(CAST(new_cases AS FLOAT)))*100 AS globalDeathPercentage
 FROM PortfolioProject..covid_deaths
 WHERE continent IS NOT NULL AND new_deaths > 0
 -- GROUP BY date
@@ -138,8 +156,7 @@ FROM PortfolioProject..covid_vaccinations AS vac
 JOIN PortfolioProject..covid_deaths AS dea
 	ON dea.location = vac.location
 	AND dea.date = vac.date
-WHERE dea.continent IS NOT NULL AND dea.location ='Albania'
---ORDER BY 2,3
+WHERE dea.continent IS NOT NULL
 )
 SELECT *, (RollingPeopleVaccinated/Population)*100 FROM PopvsVac
 
@@ -163,13 +180,10 @@ SELECT
 	dea.population, 
 	vac.new_vaccinations, 
 	SUM(CAST(vac.new_vaccinations AS FLOAT)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS rollingPeopleVaccinated
-	--MAX(rollingPeopleVaccinated / dea.population)*100
 FROM PortfolioProject..covid_vaccinations AS vac
 JOIN PortfolioProject..covid_deaths AS dea
 	ON dea.location = vac.location
 	AND dea.date = vac.date
---WHERE dea.continent IS NOT NULL
---ORDER BY 2,3
 
 
 -- Testing TEMP Table #PercentPopulationVaccinated
@@ -186,13 +200,11 @@ SELECT
 	dea.population, 
 	vac.new_vaccinations, 
 	SUM(CAST(vac.new_vaccinations AS FLOAT)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS rollingPeopleVaccinated
-	--MAX(rollingPeopleVaccinated / dea.population)*100
 FROM PortfolioProject..covid_vaccinations AS vac
 JOIN PortfolioProject..covid_deaths AS dea
 	ON dea.location = vac.location
 	AND dea.date = vac.date
 WHERE dea.continent IS NOT NULL
---ORDER BY 2,3
 
 
 
